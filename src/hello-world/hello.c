5d4a0ee66a997c8f955e764594af9a3d1ccc9905
German Gonzalez <german.gonzalez@gmail.com>
2019-01-27 21:28:41 -0300

    string sample

--- src/hello-world/hello.c
+++ src/hello-world/hello.c
@@ -1,22 +1,32 @@
 /* This is a trivial example program
    for use with CC65. */
 
-
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
 
 
-main() {  
+void main() {
    
-    printf("Hello world!\n\n" );
-    printf("free mem=%d \n\n", _heapmemavail() );
+	unsigned char string1[] =
+	{
+		97
+    };
+
+	char *string2 = "hola";
+
+	printf("Hello world!\n\n" );
+	printf("free mem=%d \n\n", _heapmemavail() );
+	printf("free mem=%d \n\n", _heapmaxavail() );
     printf("float=%d\n", sizeof(float) );
     printf("long=%d\n", sizeof(long) );
     printf("double=%d\n", sizeof(double) );
     printf("int=%d\n", sizeof(int) );
     printf("short=%d\n", sizeof(short) );
     printf("char=%d\n", sizeof(char) );
+    printf("string1=%s\n", &string1 );
+    printf("string2=%s\n", string2 );
 
     printf("\npress any key\n");
     (void) cgetc ();
